5be7aa48c7271698446fa03be4575a78219bf6c6
Jessica Bohn <bohn.16@wright.edu>
2016-05-29 02:52:41 -0500

    Initial commit & add basic vampire-esque questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,21 @@
+# Jessica Bohn
+# 05/29/2016
+# 4.4 Solo Challenge
+
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "How old are you?"
+employee_age = gets.chomp
+employee_age = employee_age.to_i
+print "What year were you born?"
+birth_year = gets.chomp
+birth_year = birth_year.to_i
+
+puts "Our company cafeteria serves garlic bread.
+		Should we order some for you?"
+likes_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
+
