b391370487816455defb4d70b06b2e82ce0c9e28
Jessica Bohn <bohn.16@wright.edu>
2016-05-29 04:10:48 -0500

    Change puts to print for base questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,19 +2,18 @@
 # 05/29/2016
 # 4.4 Solo Challenge
 
-puts "What is your name?"
+print "What is your name?"
 employee_name = gets.chomp
 
-puts "How old are you?"
+print "How old are you?"
 given_age = gets.chomp
 given_age = given_age.to_i
-print "What year were you born?"
+print "What year were you born? "
 birth_year = gets.chomp
 birth_year = birth_year.to_i
 employee_age = 2016 - birth_year
 
-puts "Our company cafeteria serves garlic bread.
-		Should we order some for you?"
+print "Our company cafeteria serves garlic bread. Should we order some for you?"
 likes_garlic = gets.chomp
 if likes_garlic == "yes"
 	likes_garlic = true
@@ -22,7 +21,7 @@ else
 	likes_garlic = false
 end
 
-puts "Would you like to enroll in the company's health insurance?"
+print "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
 if health_insurance == "yes"
 	health_insurance = true
@@ -30,6 +29,8 @@ else
 	health_insurance = false
 end
 
-when given_age == employee_age && (likes_garlic || health_insurance)
+if given_age == employee_age && (likes_garlic || health_insurance)
 	puts "Probably not a vampire."
+else
+	puts "I don't know"
 end
