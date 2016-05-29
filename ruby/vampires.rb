3d94412e4424b9c8caec764cca4a945e7c35157e
Jessica Bohn <bohn.16@wright.edu>
2016-05-29 04:53:55 -0500

    Fix age range calculation

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,10 +8,13 @@ employee_name = gets.chomp
 print "How old are you?"
 given_age = gets.chomp
 given_age = given_age.to_i
+p given_age
 print "What year were you born? "
 birth_year = gets.chomp
 birth_year = birth_year.to_i
 employee_age = 2016 - birth_year
+employee_age = (employee_age-1..employee_age)
+p employee_age
 
 print "Our company cafeteria serves garlic bread. Should we order some for you?"
 likes_garlic = gets.chomp
@@ -29,8 +32,4 @@ else
 	health_insurance = false
 end
 
-if given_age == employee_age && (likes_garlic || health_insurance)
-	puts "Probably not a vampire."
-else
-	puts "I don't know"
-end
+p employee_age === given_age
