d8c7919cc7456f0e481a3d4e186d0e3cc5c9ab78
Jessica Bohn <bohn.16@wright.edu>
2016-06-03 21:30:58 -0500

    Initial commit - establish blank array --> add five items to array

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,8 @@
+# Jessica Bohn & Kimberly Patton
+# 06/03/2016
+
+books = []
+p books
+
+books = ["Harry Potter", "The Great Gatsby", "The Lord of the Rings", "The Hunger Games", "Pride & Prejudice"]
+p books
