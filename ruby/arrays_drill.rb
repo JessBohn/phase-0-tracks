bb9bfd454a9750978e058bd0d7c7997e0eb0ad56
Jessica Bohn <bohn.16@wright.edu>
2016-06-03 22:34:58 -0500

    Add information from Kimberly's document

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,6 +1,14 @@
 # Jessica Bohn & Kimberly Patton
 # 06/03/2016
 
+def method_of_arrays(array, y)
+    new_array = array << y
+end
+
+def build_array(x, y, z)
+    array = [x, y, z]
+end    
+
 books = []
 p books
 
@@ -17,9 +25,16 @@ books.shift
 p books
 
 p "The book The Great Gatsby appears in this array: #{books.include?("The Great Gatsby")}"
-# p books.bsearch {|x| x == "The Hunger Games"}
+
 
 colors = ["blue", "pink", "green"]
 
 books_and_colors = books + colors
 p books_and_colors
+
+p build_array(1, "two", nil)
+
+
+p method_of_arrays([], "Hi")
+p method_of_arrays([3, 5], 1)
+p method_of_arrays(["Hello"], 7)
