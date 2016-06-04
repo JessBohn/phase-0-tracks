eab1b3b42d0ce1fcb837c48fd188de64a4913e3f
Jessica Bohn <bohn.16@wright.edu>
2016-06-03 21:39:46 -0500

    Delete then add item at index 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -6,3 +6,9 @@ p books
 
 books = ["Harry Potter", "The Great Gatsby", "The Lord of the Rings", "The Hunger Games", "Pride & Prejudice"]
 p books
+
+books.delete_at(2)
+p books
+
+books.insert(2, "The Adventures of Huckleberry Finn")
+p books
