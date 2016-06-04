3cacb08b012e3af9fb777113ad9963dc80291529
Jessica Bohn <bohn.16@wright.edu>
2016-06-03 21:59:44 -0500

    Remove first item, add search for specific item, create new array, add btoh arrays together

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -12,3 +12,14 @@ p books
 
 books.insert(2, "The Adventures of Huckleberry Finn")
 p books
+
+books.shift 
+p books
+
+p "The book The Great Gatsby appears in this array: #{books.include?("The Great Gatsby")}"
+# p books.bsearch {|x| x == "The Hunger Games"}
+
+colors = ["blue", "pink", "green"]
+
+books_and_colors = books + colors
+p books_and_colors
