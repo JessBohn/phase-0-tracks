686652e4292945398c372b6ca6b2eb9e537f57dd
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 02:55:45 -0500

    Define method for advancing only vowels in name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,8 +6,28 @@
 # change each of its vowels to the next one in the order [aeiou]
 # change the consonants to the next in the alphabet
 
-agent_real_name = "felicia torres"
-names_separated = agent_real_name.split(' ')
+agent_real_name = "Felicia Torres".downcase
+agent_real_name = agent_real_name.split(' ')
 
 # Use the .reverse method to swap the order of the items (names) in the array
-last_first = names_separated.reverse
+p last_first = agent_real_name.reverse
+ p last_first = last_first.join('')
+# p vowels = combine_names.scan(/[aeiou]/)
+# vowels = vowels.to_s
+# p new_vowels = vowels.next(/[aeiou]/)
+
+# Define method to take the vowels in the name and switcht them to the next in the vowel order [aeiou]
+# def next_vowel(x)
+# 	vowels = ["a", "e", "i", "o", "u"]
+# 	last_first = last_first.split('')
+# 	go_to_next_vowel = last_first.map do |letter|
+# 		if vowels.include?(letter)
+# 			vowels.rotate(1)[vowels.index(letter)]
+# 		else
+# 			letter
+# 		end
+# 	end
+# 	p go_to_next_vowel.join
+# end
+
+# p next_vowel(last_first)
