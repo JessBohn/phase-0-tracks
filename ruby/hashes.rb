4848d9fe1b7f6d82a95c39106dbca6998e5231df
Jessica Bohn <bohn.16@wright.edu>
2016-06-05 21:30:38 -0500

    Initial commit - initial pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+# Jessica Bohn
+# 06/05/2016
+
+# I will be writing an application for a pet sitter
+
+# Must implement all important questions 
+	# Name
+	# Age
+	# Address
+	# All-time favorite animal
+	# How many pets do they have
+	# What kind of pets do they have
+	# Have they ever watched someone else's pet
+	# End - whether they can be my pet sitter or not
+
