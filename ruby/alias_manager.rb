3199ee7fb0083ae445f0ef04245fbbb3ea675adc
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 03:37:18 -0500

    Combined vowel and consonant methods into one method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -13,24 +13,72 @@ agent_real_name = agent_real_name.split(' ')
 p last_first = agent_real_name.reverse
 p last_first = last_first.join(' ')
 
-# Define method to take the vowels in the name and switcht them to the next in the vowel order [aeiou]
+# Define method to take the vowels in the name and switch them to the next in the vowel order [aeiou]
+	# Establish array containing the vowels
+	# Compares letters from the given name to the array of vowels
+	# If a vowel
+		# Switch the vowels to the next one in the order
+	# Else
+		# Keep original letters
+	# Rejoin the letters to form a name
+# def next_vowel(str)
+#   vowels = ["a", "e", "i", "o", "u"]
+#   str = str.split('')
+#   name_adv_vowels = str.map do |letter|
+#     if vowels.include?(letter)
+#       vowels.rotate(1)[vowels.index(letter)]
+#     else
+#       letter
+#     end
+#   end
+#   name_adv_vowels.join
+# end
+# p next_vowel(last_first)
 
-def next_vowel(str)
-  vowels = ["a", "e", "i", "o", "u"]
-  str = str.split('')
-  name_adv_vowels = str.map do |letter|
-    if vowels.include?(letter)
-      vowels.rotate(1)[vowels.index(letter)]
-    else
-      letter
-    end
-  end
-  name_adv_vowels.join
-end
-p next_vowel(last_first)
+# Define method to take the consonants in the name and switch them to the next in the order
+	# Establish array containing the vowels
+		# Cons = whole alphabet - vowels
+	# Compares letters from the given name to the array of cons
+	# If a con
+		# Switch the cons to the next one in the order
+	# Else
+		# Keep original letters
+	# Rejoin the letters to form a name
+# def next_consonant(str)
+# 	alphabet = ("a".."z").to_a
+# 	vowels = ["a", "e", "i", "o", "u"]
+# 	consonants = alphabet - vowels
+# 	str = str.split('')
+#   	name_adv_cons = str.map do |letter|
+# 	    if consonants.include?(letter)
+# 	      consonants.rotate(1)[consonants.index(letter)]
+# 	    else
+# 	      letter
+# 	    end
+#   end
+#   name_adv_cons.join
+# end
 
-def next_consonant(str)
+# p next_consonant(next_vowel(last_first))
+
+# new_agent_name = next_consonant(next_vowel(last_first))
+# p proper_new_agent_name = new_agent_name.capitalize!
+
+def new_agent_name(str)
 	alphabet = ("a".."z").to_a
 	vowels = ["a", "e", "i", "o", "u"]
 	consonants = alphabet - vowels
+	str = str.split('')
+  	name_adv_letters = str.map do |letter|
+	    if vowels.include?(letter)
+      		vowels.rotate(1)[vowels.index(letter)]
+	    elsif consonants.include?(letter)
+	      consonants.rotate(1)[consonants.index(letter)]
+	  	else
+	  		letter
+	    end
+  end
+  name_adv_letters.join
 end
+
+p new_agent_name(last_first)
