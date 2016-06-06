dd613c958d0c3b0e95e568a537e7b206e2e4d244
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 02:03:24 -0500

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+# Jessica Bohn
+# 06/06/2016
+
+# Program as a whole must take the name of an agent, 
+# Swap first and last name
+# change each of its vowels to the next one in the order [aeiou]
+# change the consonants to the next in the alphabet
+
+
