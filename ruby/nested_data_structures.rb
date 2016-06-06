e014b87326e47be7c62ef3301db19038018be4ba
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 00:35:07 -0500

    Initial commit - create hash for house - add four rooms with arrays of items

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,14 @@
+# Jessica Bohn
+# 05/07/16
+
+# 5.5 : Nested Data Structures
+
+# I am going to be cheesy and take the idea from Jennifer's idea and model my house in this nested data Structures
+
+the_bohn_residence = {
+	'Living Room' => ['2-piece recliner', 'wobbly fan', 'television', 'James'],
+	'Kitchen' => ['leaky refrigerator', 'electric stove', 'pantry'],
+	'Julie Office' => ['large fancy desk', 'small fancy desk', 'television', 'supply cabinet', 'files'],
+	'Garage' => ['junk', 'refrigerator', 'Jeep Wrangler'],
+
+}
