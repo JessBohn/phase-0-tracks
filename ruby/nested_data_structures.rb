b0d2ddac2e1d89d09d46345e44f18121b6f304bb
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 00:47:16 -0500

    Add Jess Room with nested array

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -6,9 +6,36 @@
 # I am going to be cheesy and take the idea from Jennifer's idea and model my house in this nested data Structures
 
 the_bohn_residence = {
-	'Living Room' => ['2-piece recliner', 'wobbly fan', 'television', 'James'],
-	'Kitchen' => ['leaky refrigerator', 'electric stove', 'pantry'],
-	'Julie Office' => ['large fancy desk', 'small fancy desk', 'television', 'supply cabinet', 'files'],
-	'Garage' => ['junk', 'refrigerator', 'Jeep Wrangler'],
+	'Living Room' => [
+		'2-piece recliner', 
+		'wobbly fan', 
+		'television', 
+		'James'],
+	'Kitchen' => [
+		'leaky refrigerator',
+		 'electric stove',
+		 'pantry'],
+	'Julie Office' => ['
+		large fancy desk',
+		 'small fancy desk',
+		 'television', 
+		 'supply cabinet', 
+		 'files'],
+	'Garage' => [
+		'junk', 
+		'refrigerator', 
+		'Jeep Wrangler'],
+
+	'Jess Room' => [
+		'queen size bed', 
+		['Lambda Lambda Lambda', 
+			'The Great Catsby'], 
+		'37-inch TV', 
+		'Xbox One', 
+		'unfolded laundry']
 
 }
+
+p the_bohn_residence
+
+p the_bohn_residence['Jess Room'][1][0]
