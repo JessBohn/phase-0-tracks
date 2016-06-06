f9c4d966b860eea26be4a925d0d49ce73053b366
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 03:05:59 -0500

    Fix next_vowel to actually work

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,23 +11,20 @@ agent_real_name = agent_real_name.split(' ')
 
 # Use the .reverse method to swap the order of the items (names) in the array
 p last_first = agent_real_name.reverse
- p last_first = last_first.join('')
-# p vowels = combine_names.scan(/[aeiou]/)
-# vowels = vowels.to_s
-# p new_vowels = vowels.next(/[aeiou]/)
+p last_first = last_first.join(' ')
 
 # Define method to take the vowels in the name and switcht them to the next in the vowel order [aeiou]
-# def next_vowel(x)
-# 	vowels = ["a", "e", "i", "o", "u"]
-# 	last_first = last_first.split('')
-# 	go_to_next_vowel = last_first.map do |letter|
-# 		if vowels.include?(letter)
-# 			vowels.rotate(1)[vowels.index(letter)]
-# 		else
-# 			letter
-# 		end
-# 	end
-# 	p go_to_next_vowel.join
-# end
 
-# p next_vowel(last_first)
+def next_vowel(str)
+  vowels = ["a", "e", "i", "o", "u"]
+  str = str.split('')
+  name_adv_vowels = str.map do |vowel|
+    if vowels.include?(vowel)
+      vowels.rotate(1)[vowels.index(vowel)]
+    else
+      vowel
+    end
+  end
+  name_adv_vowels.join
+end
+p next_vowel(last_first)
