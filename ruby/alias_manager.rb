d876638c21ad0014533078c6da0ee72a9043fce8
Jessica Bohn <bohn.16@wright.edu>
2016-06-06 02:16:36 -0500

    Separate name into first and last, reverse order using .reverse on array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,4 +6,8 @@
 # change each of its vowels to the next one in the order [aeiou]
 # change the consonants to the next in the alphabet
 
+agent_real_name = "felicia torres"
+names_separated = agent_real_name.split(' ')
 
+# Use the .reverse method to swap the order of the items (names) in the array
+last_first = names_separated.reverse
