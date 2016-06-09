b25ba1eb88c04cc2d6b9a3e70a1c9bd34a93f0ae
Jessica Bohn <bohn.16@wright.edu>
2016-06-08 19:59:26 -0500

    Copy code from Melanie's file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -37,3 +37,31 @@ sparky.speak(4)
 sparky.roll_over
 sparky.dog_years(5)
 sparky.sit
+
+class Rabbit
+  def initialize
+    puts "Here comes another rabbit!"
+  end
+  def hop(x)
+    x.times {print "*hop*"}
+  end
+  def eat_carrots(y)
+    print " ate #{y} carrots!"
+    puts ""
+  end
+end
+
+rabbits = Array.new
+num_rabbits = 0
+while num_rabbits < 50
+  rabbits[num_rabbits] = Rabbit.new
+  num_rabbits += 1
+end
+rabbits.each do |i|
+  puts "Rabbit number #{rabbits.index(i)}"
+  i.hop(2)
+end
+rabbits.each do |n|
+  print "Rabbit number #{rabbits.index(n)}"
+  n.eat_carrots(4)
+end
