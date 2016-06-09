9b394407650a5f009b116eef61199517fe43b9fd
Jessica Bohn <bohn.16@wright.edu>
2016-06-08 19:26:38 -0500

    Add dog years methods - calc human years -> dog year : add sit method and ininitialize

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,6 @@
 class Puppy
 
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -10,9 +11,29 @@ class Puppy
   	x.times { puts "Woof!" }
   end
 
+  def roll_over
+  	puts "*roll over*"
+  end
+
+  def dog_years(human_years)
+  	dog_year = human_years * 7
+  	puts "This dog is #{dog_year} years old in dog years!"
+  end
+
+  def sit
+  	puts "*sits*"
+  end
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+
 end
 
 sparky = Puppy.new
 sparky.fetch("ball")
 
 sparky.speak(4)
+sparky.roll_over
+sparky.dog_years(5)
+sparky.sit
