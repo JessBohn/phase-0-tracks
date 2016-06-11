fabc9802879b6c8ea8fa5d1d14dc2380d92ad049
Jessica Bohn <bohn.16@wright.edu>
2016-06-11 00:21:01 -0500

    Define method for milk and cookies - returns sentence using cookie given

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -11,8 +11,11 @@ class Santa
 	end
 		
 	# Define method for eating milk and cookies
+	def eat_milk_and_cookies(cookie)
 		# Takes in a type of cookie as a parameter
 		# Prints the cookie in a sentence
+		print "That was a good #{cookie}! The milk was delicious too!"
+	end
 
 	# Add initiliaze method
 		# Prints line stating new Santa instance
