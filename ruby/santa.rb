3d004277ad1f5a04b9ed5a044dfdd3fbb12ba902
Jessica Bohn <bohn.16@wright.edu>
2016-06-11 00:14:32 -0500

    Initial commit for 6-3 (wrong branch)

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+# Jessica Bohn
+# 06/11/16
+
+# Create a Santa Class
+class Santa
+
+	# Define method for speak
+		# Prints holiday greeting
+
+	# Define method for eating milk and cookies
+		# Takes in a type of cookie as a parameter
+		# Prints the cookie in a sentence
+
+	# Add initiliaze method
+		# Prints line stating new Santa instance
+end
