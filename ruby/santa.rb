2259892995f175e7ec4164f8566d179d249c5a97
Jessica Bohn <bohn.16@wright.edu>
2016-06-11 01:02:51 -0500

    Add attributes of gender & ethnicity to initialize

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,22 +5,28 @@
 class Santa
 	# Add initiliaze method
 		# Prints line stating new Santa instance
-	def initialize
-		print "Initializing Santa instance..."
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		puts "Initializing Santa instance..."
 	end
 
 	# Define method for speak
 	def speak
 		# Prints holiday greeting
-		print "Ho, ho, ho! Haaaappy holidays!"
+		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 		
 	# Define method for eating milk and cookies
 	def eat_milk_and_cookies(cookie)
 		# Takes in a type of cookie as a parameter
 		# Prints the cookie in a sentence
-		print "That was a good #{cookie}! The milk was delicious too!"
+		puts "That was a good #{cookie}! The milk was delicious too!"
+		puts "#{@gender} #{@ethnicity}"
 	end
 
-
 end
+
+santa = Santa.new("female", "Latino")
+santa.speak
+santa.eat_milk_and_cookies("chocolate chip cookie")
