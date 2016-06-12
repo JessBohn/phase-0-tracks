c57dd21631ec37f4b315f57405d536fdb9ae38fc
Jessica Bohn <bohn.16@wright.edu>
2016-06-12 08:12:47 -0500

    Comment out orig. code - create mixin vers. of Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,14 +2,25 @@
 # 06/12/16
 
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yell_happily(words)
-  	words + "!!!" + " :D "
-  end
+	def loud_yell(words)
+		puts words.upcase + "!!!"
+	end
 end
 
-p Shout.yell_happily("I love my cats")
-p Shout.yell_angrily("My feet hurt")
+class Child
+	include Shout
+end
+
+
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#   	words + "!!!" + " :D "
+#   end
+# end
+
+# p Shout.yell_happily("I love my cats")
+# p Shout.yell_angrily("My feet hurt")
