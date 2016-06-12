c4eaded9ef1ef3cdef2e260a8199a78836ee1952
Jessica Bohn <bohn.16@wright.edu>
2016-06-12 05:05:28 -0500

    Change attribute collections to user inputted

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,33 +1,40 @@
 # Jessica Bohn
 # 06/12/16
 
+#BUSINESS LOGIC
+
 # Create a class that creates a cute kitty cat!
 class Cat
-	attr_reader :breed, :age, :gender, :meowiness
+	attr_reader :breed, :age, :meowiness
+	attr_accessor :gender
 	# Set characteristics in initialize 
 	def initialize
 		puts "Initializing cat instance..."
+		# Set characteristic for the cat's name
+			# Ask user for cat's name
+			puts "What is this cat's name?"
+			@name = gets.chomp
 		# Set characteristic for breed of each cat
-			# Create a list of different cat breeds
-			cat_breeds = ["Siamese", "calico", "diluted calico", 
-				"Maine Coon", "Bengal", "Sphynx", "Russian Blue", 
-				"Himalayan", "Munchkin", "Siberian"]
-			# Choose random breed from list for each rendering
-			@breed = cat_breeds.sample
-
-		# Set characteristic for cat's age
-			# Choose random age between 0 and 20 (cat's don't get that old)
-			@age = Random.new.rand(20)
+			# Ask user for the breed of their cat
+			puts "What breed is #{@name}?"
+			@breed = gets.chomp
 
 		# Set characteristic for gender
 			# create list of genders (male & female)
 			genders = ["female", "male"]
 			# randomly select gender fromt list to assign
 			@gender = genders.sample
+			
+		# Set characteristic for cat's age
+			# Ask user for the age of the cat
+			puts "How old is #{@name}?"
+			age = gets.chomp
+			@age = age.to_i
+
 
 		# Set characteristic for level of talkative-ness
 			# Create list of "levels" - mild, medium, high
-			meowiness = ["mild", "medium", "high"]
+			meowiness = ["low", "medium", "high"]
 			# Randomly select level from list
 			@meowiness = meowiness.sample
 
@@ -40,7 +47,7 @@ class Cat
 		def meow
 		# output: prints string of # of meows based on talkative-ness
 			# IF mild , meows once
-			if @meowiness == "mild"
+			if @meowiness == "low"
 				puts "meowww"
 			# IF medium, meows 3 times
 			elsif @meowiness == "medium"
@@ -68,7 +75,88 @@ class Cat
 		end
 end
 
-lambda = Cat.new
-lambda.meow
-lambda.catnip_attack
-lambda.cuddles
+# USER INTERFACE
+
+# User needs to be able to create as many instances as they want
+	# Keep creating instances until the user types 'done'
+		# Ask user for the name of the cat
+		# Ask user for the breed
+		# Ask user for age
+			# convert to a number
+		# Ask if the cat is female or male
+		# Ask for the cat's level of meowiness (low, medium, or high)
+		# Add each cat created to an array
+	# Print out all attributes of each cat created
+
+
+Cat.new
+
+
+# Original class code w/o user INTERFACE
+
+# class Cat
+# 	attr_reader :breed, :age, :meowiness
+# 	attr_accessor :gender
+# 	# Set characteristics in initialize 
+# 	def initialize
+# 		puts "Initializing cat instance..."
+# 		# Set characteristic for breed of each cat
+# 			# Create a list of different cat breeds
+# 			cat_breeds = ["Siamese", "calico", "diluted calico", 
+# 				"Maine Coon", "Bengal", "Sphynx", "Russian Blue", 
+# 				"Himalayan", "Munchkin", "Siberian"]
+# 			# Choose random breed from list for each rendering
+# 			@breed = cat_breeds.sample
+
+# 		# Set characteristic for cat's age
+# 			# Choose random age between 0 and 20 (cat's don't get that old)
+# 			@age = Random.new.rand(20)
+
+# 		# Set characteristic for gender
+# 			# create list of genders (male & female)
+# 			genders = ["female", "male"]
+# 			# randomly select gender fromt list to assign
+# 			@gender = genders.sample
+
+# 		# Set characteristic for level of talkative-ness
+# 			# Create list of "levels" - mild, medium, high
+# 			meowiness = ["low", "medium", "high"]
+# 			# Randomly select level from list
+# 			@meowiness = meowiness.sample
+
+# 			puts "This cat is a #{@breed}, is a #{@gender}, and is #{@age} years old!"
+# 			puts "This cat is also a #{@meowiness} meow-er!"
+# 		end
+# 		meowiness = @meowiness
+# 	# Method for meowing
+# 		# Input: level of talkative-ness
+# 		def meow
+# 		# output: prints string of # of meows based on talkative-ness
+# 			# IF mild , meows once
+# 			if @meowiness == "low"
+# 				puts "meowww"
+# 			# IF medium, meows 3 times
+# 			elsif @meowiness == "medium"
+# 				3.times { print "meowww  "}
+# 			# IF high, meows 5 times, CAPS
+# 			else
+# 				5.times { print "meowww  ".upcase }
+# 			end
+
+# 		end
+
+# 	# Method for catnip attack
+# 		def catnip_attack
+# 		# prints string announcing cat has found catnip and is now
+# 		# bouncing off the walls
+# 			puts "Uh oh the cat has found some catnip and is going crazy!"
+# 		end
+
+# 	# Method cuddles
+# 		def cuddles
+# 		# prints string announcing your baby kitty has jumped on you
+# 		# for cuddle time! & is now kneading you, ouch
+# 			puts "Aww, the cat has climbed on your lap and is cuddling with you."
+# 			puts "Oh, they're kneading you. Ouch."
+# 		end
+# end
