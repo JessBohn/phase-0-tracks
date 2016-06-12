77e8399b3f741a21435883de9dce682b16fe1d67
Jessica Bohn <bohn.16@wright.edu>
2016-06-12 03:12:49 -0500

    Design cat class with pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,34 @@
+# Jessica Bohn
+# 06/12/16
+
+# Create a class that creates a cute kitty cat!
+	# Set characteristics in initialize 
+		# Set characteristic for breed of each cat
+			# Create a list of different cat breeds
+			# Choose random breed from list for each rendering
+
+		# Set characteristic for cat's age
+			# Choose random age between 0 and 20 (cat's don't get that old)
+		
+		# Set characteristic for gender
+			# create list of genders (male & female)
+			# randomly select gender fromt list to assign
+
+		# Set characteristic for level of talkative-ness
+			# Create list of "levels" - mild, medium, high
+
+	# Method for meowing
+		# Input: level of talkative-ness
+		# output: prints string of # of meows based on talkative-ness
+			# IF mild , meows once
+			# IF medium, meows 3 times
+			# IF high, meows 5 times, CAPS
+
+	# Method for catnip attack
+		# prints string announcing cat has found catnip and is now
+		# bouncing off the walls
+
+	# Method cuddles
+		# prints string announcing your baby kitty has jumped on you
+		# for cuddle time! & is now kneading you, ouch
+
