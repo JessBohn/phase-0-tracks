ffd628e175779752394c0c8fb7ddf6b196501952
Jessica Bohn <bohn.16@wright.edu>
2016-06-12 07:59:32 -0500

    Add module method for yelling happily + driver code to test both methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -5,4 +5,11 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yell_happily(words)
+  	words + "!!!" + " :D "
+  end
 end
+
+p Shout.yell_happily("I love my cats")
+p Shout.yell_angrily("My feet hurt")
