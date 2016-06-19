e0320c3ff94ad040fcd406711c079a615ba667a4
Jessica Bohn <bohn.16@wright.edu>
2016-06-19 04:35:08 -0500

    Pseudocode function to reverse a string

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,11 @@
 // Jessica Bohn
 // 06/19/16
+
+// Set first variable to empty string
+// Set second variable to number of characters in string
+// WHILE second variable does not equal zero
+  // print character in string equal to second variable's value place in string
+  // reset first variable to itself plus the character the place value of the second variable
+  // set second variable value lower by one increment
+// print final value of the first variable
+
