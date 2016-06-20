1d29d6918ec55c4115c510c7f71b8f38e9504354
Jessica Bohn <bohn.16@wright.edu>
2016-06-20 03:41:34 -0500

    Pseudocode release 0 - function to find longest phrase in array

---
+++ js/algos.js
@@ -0,0 +1,17 @@
+// Jessica Bohn
+// 06/20/16
+// 7.3 - Solo Challenge
+
+// Release 0
+
+  // Input list of different phrases
+  // Calculate number of characters in each phrase
+  // Set variable for number of characters in string, numCharacters, to zero
+  // Set index variable, i, to zero
+    // FOR i less than the length of the array
+      // IF numCharacters is less than the length of string in array at i
+        // Set numCharacters to length of string in array at i
+      // END
+      // Increase value of i by 1
+    // ENDFOR
+  // Print string at index of value i where numCharacters is max for the set
