bb41c3dab1ee79d2821c5a72d9e786080749f568
Jessica Bohn <bohn.16@wright.edu>
2016-06-19 21:46:42 -0500

    Add constructor function to create car objects

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,24 @@ for (var i = 0; i < horses.length; i++){
  }
 
 console.log(object)
+
+function Car(brand, year, isBroken){
+  this.brand = brand;
+  this.year = year;
+  this.isBroken = isBroken;
+  this.honk = function() {
+    if (this.isBroken) {
+      console.log("Buy a new car!");
+    } else {
+      console.log("You're good to go!");
+    }   
+  };
+}
+
+var car = new Car("Toyota", 1987, false)
+console.log(car);
+car.honk()
+
+var car2 = new Car("Kia", 2001, true)
+console.log(car2);
+car2.honk()
