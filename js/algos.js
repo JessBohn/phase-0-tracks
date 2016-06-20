b4b84336ce00ed031546ee56f6de90e574c07467
Jessica Bohn <bohn.16@wright.edu>
2016-06-20 03:54:33 -0500

    Add for loop to find the longest phrase in an array

--- js/algos.js
+++ js/algos.js
@@ -5,13 +5,19 @@
 // Release 0
 
   // Input list of different phrases
-  // Calculate number of characters in each phrase
-  // Set variable for number of characters in string, numCharacters, to zero
+  var phrases = ["you're a cutie", "hello handsome", 
+  "hi handsome pants", "what's up love"]
+  // Set variable for number of characters in string, longestPhrase, to empty string
+  var longestPhrase = "";
   // Set index variable, i, to zero
-    // FOR i less than the length of the array
-      // IF numCharacters is less than the length of string in array at i
-        // Set numCharacters to length of string in array at i
-      // END
-      // Increase value of i by 1
-    // ENDFOR
-  // Print string at index of value i where numCharacters is max for the set
+  // FOR i less than the length of the array
+    // Increase value of i by 1
+  for (var i = 0; i < phrases.length; i++) {
+  // IF longestPhrase is less than the length of string in array at i
+  if ( phrases[i].length > longestPhrase.length ) {
+      // Set longestPhrase to length of string in array at i
+      longestPhrase = phrases[i];
+    }
+  }
+  // Print string at index of value i where longestPhrase is max for the set
+  console.log(longestPhrase)
