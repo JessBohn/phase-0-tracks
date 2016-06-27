b1fd19f659ff54cab8b93b45cce9350dcfa4b93a
Jessica Bohn <bohn.16@wright.edu>
2016-06-27 00:37:57 -0500

    Require sqlite3 & create database for movies

--- databases/movie_directory/movie_directory.rb
+++ databases/movie_directory/movie_directory.rb
@@ -2,3 +2,15 @@
 # 6/26/16
 # 8.5 Challenge!
 
+# Create a command-line input guided movie database for an individual
+# Separate table for each category of movie
+# Should allow user to sort alphabetically
+
+# Use the sqlite3 add-on to properly run program
+require 'sqlite3'
+require 'faker'
+
+# Create database to store the movie information
+db = SQLite3::Database.new("movies.db")
+
+
