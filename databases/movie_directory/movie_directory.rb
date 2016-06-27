a5d55517953e1f26455154e797ba1695fbe63633
Jessica Bohn <bohn.16@wright.edu>
2016-06-27 01:29:45 -0500

    Add p-code for user input to collect movie data

--- databases/movie_directory/movie_directory.rb
+++ databases/movie_directory/movie_directory.rb
@@ -90,8 +90,63 @@ db.execute(create_action_cmd)
 db.execute(create_scifi_cmd)
 db.execute(create_drama_cmd)
 
+#USER INPUT
 
-# Ask user for category of movie to be entered into collection
+# Run movie data collection until user enters done
+
+  # Ask user for category of movie to be entered into collection
+  puts "What is the category of the movie?"
+  # Collect and covert category to lowercase letters
+  category = gets.chomp.downcase
+  # Collect movie title from user
+  puts "What is the title of the movie?"
+  title = gets.chomp
+  # Collect movie director from user
+  puts "What is the name of this movie's director?"
+  director = gets.chomp
+  # Collect release date from user and convert to a number
+  puts "What year was this movie released?"
+  release_year = gets.chomp.to_i
+
+  # IF comedy
+    # Insert information into comedy table
+  # ELSIF horror
+    # Insert information into horror table
+  # ELSIF romance
+    # Insert information into romance table
+  # ELSIF action
+    # Insert information into action table
+  # ELSIF scifi
+    # Insert information into scifi table
+  # ELSIF drama
+    # Insert information into drama table
+  # ELSE
+    # Tell user category unknown
+    # List available categories
+    # Ask user for movie category
+  # END
+
+# Repeat display method until user says exit
+  # Ask user if they would like to view their lists
+    # IF yes
+      # Ask which category they would like to see
+      # IF all
+        # Display all
+      # ELSIF comedy
+        # display comedy table
+      # ELSIF horror
+        # display horro table
+      # ELSIF romance
+        # display romance table
+      # ELSIF action
+        # display action
+      # ELSIF scifi table
+        # display scifi table
+      # ELSIF drama
+        # display drama table
+      # ELSE
+        # Tell them to pick one of the categories or say all
+      # END
 
 
 
